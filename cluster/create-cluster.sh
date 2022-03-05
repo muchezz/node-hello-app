c04d9a695f7a0604dee367667c5ad0d80b90ca09
Mucheru <mucheru@outlook.com>
2022-03-05 16:48:35 +0300

    Adding Keypair

--- cluster/create-cluster.sh
+++ cluster/create-cluster.sh
@@ -30,7 +30,7 @@ aws iam create-role --role-name AmazonEKSFargatePodExecutionRole --assume-role-p
 aws iam attach-role-policy --role-name AmazonEKSFargatePodExecutionRole --policy-arn arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy
 
 #create the EKS Fargate cluster
-eksctl create cluster --name $CLUSTER_NAME --version 1.18 --fargate --ssh-access=true --ssh-public-key=ponea --managed --spot --name spot-4vcpu-16gb --asg-access --nodes-max 20
+eksctl create cluster --name $CLUSTER_NAME --version 1.18 --fargate --ssh-access=true --ssh-public-key=ponea-eks --managed --spot --name spot-4vcpu-16gb --asg-access --nodes-max 20
 
 #https://aws.amazon.com/blogs/opensource/kubernetes-ingress-aws-alb-ingress-controller/
 #https://docs.aws.amazon.com/eks/latest/userguide/enable-iam-roles-for-service-accounts.html
